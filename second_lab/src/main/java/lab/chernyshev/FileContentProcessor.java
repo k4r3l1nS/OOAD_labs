6dc1f95defcf99fb3c6d3dabfd5e4116c79d93ec
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2024-02-17 22:41:50 +0300

    Added scanner

--- second_lab/src/main/java/lab/chernyshev/FileContentProcessor.java
+++ second_lab/src/main/java/lab/chernyshev/FileContentProcessor.java
@@ -4,9 +4,12 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 
 public abstract class FileContentProcessor {
+
     private static final String path = "second_lab/src/main/resources/";
+
     protected abstract String processingMethod(
             FileReader file1,
             FileReader file2,
@@ -23,15 +26,29 @@ public abstract class FileContentProcessor {
             String fileName4
     ) {
         try {
+
             FileReader fileReader1 = new FileReader(Paths.get(path, fileName1).toString());
             FileReader fileReader2 = new FileReader(Paths.get(path, fileName2).toString());
             FileReader fileReader3 = new FileReader(Paths.get(path, fileName3).toString());
+
             FileWriter fileWriter = new FileWriter(Paths.get(path, fileName4).toString());
-            fileWriter.write(processingMethod(fileReader1, fileReader2, fileReader3, fileName1, fileName2, fileName3));
+            fileWriter.write(
+                    processingMethod(
+                            fileReader1,
+                            fileReader2,
+                            fileReader3,
+                            fileName1,
+                            fileName2,
+                            fileName3
+                    )
+            );
+
             fileReader1.close();
             fileReader2.close();
             fileReader3.close();
             fileWriter.close();
+
+            System.out.println(LocalDateTime.now() + " | Файлы обработаны успешно");
         } catch (IOException ex) {
             System.out.println("Ошибка ввода/вывода");
         }
--- second_lab/src/main/java/lab/chernyshev/SecondLabApplication.java
+++ second_lab/src/main/java/lab/chernyshev/SecondLabApplication.java
@@ -1,22 +1,52 @@
 package lab.chernyshev;
 
+import java.util.Scanner;
+
 public class SecondLabApplication {
+
+    private static final int FILE_COUNT = 4;
+
+    private static final String SOURCE_DIRECTORY_PATH = "source/";
+    private static final String OUTPUT_DIRECTORY_PATH = "output/";
+
+    private static final String FILENAMES_EXAMPLE1 = "file1.txt file2.txt file3.txt output1.txt";
+    private static final String FILENAMES_EXAMPLE2 = "file1.txt file2.txt file3.txt output2.txt";
 
     public static void main(String[] args) {
 
+        // Подразумеваемый лабой способ - ввод с консоли
+//        Scanner scanner = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(FILENAMES_EXAMPLE1);
+        String[] filenames = scanFilenames(scanner);
+
         FileContentProcessor fileContentProcessor1 = new ApplicationOne();
         fileContentProcessor1.processFileContent(
-                "source/file1.txt",
-                "source/file2.txt",
-                "source/file3.txt",
-                "output/ApplicationOneOutput.txt"
+                SOURCE_DIRECTORY_PATH + filenames[0],
+                SOURCE_DIRECTORY_PATH + filenames[1],
+                SOURCE_DIRECTORY_PATH + filenames[2],
+                OUTPUT_DIRECTORY_PATH + filenames[3]
         );
+
+        scanner = new Scanner(FILENAMES_EXAMPLE2);
+        filenames = scanFilenames(scanner);
 
         FileContentProcessor fileContentProcessor2 = new ApplicationTwo();
         fileContentProcessor2.processFileContent(
-                "source/file1.txt",
-                "source/file2.txt",
-                "source/file3.txt",
-                "output/ApplicationTwoOutput.txt");
+                SOURCE_DIRECTORY_PATH + filenames[0],
+                SOURCE_DIRECTORY_PATH + filenames[1],
+                SOURCE_DIRECTORY_PATH + filenames[2],
+                OUTPUT_DIRECTORY_PATH + filenames[3]
+        );
+    }
+
+    private static String[] scanFilenames(Scanner scanner) {
+
+        String[] filenames = new String[FILE_COUNT];
+        for (int index = 0; index < FILE_COUNT && scanner.hasNext(); ++index) {
+            filenames[index] = scanner.next();
+        }
+
+        return filenames;
     }
 }
