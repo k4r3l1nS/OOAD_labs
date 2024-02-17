b751218eb10b026c6d751b0496df357e5731fb63
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2024-02-17 22:17:38 +0300

    Fixed exception handling, minor code refactoring changes

--- second_lab/src/main/java/lab/chernyshev/FileContentProcessor.java
+++ second_lab/src/main/java/lab/chernyshev/FileContentProcessor.java
@@ -7,18 +7,33 @@ import java.nio.file.Paths;
 
 public abstract class FileContentProcessor {
     private static final String path = "second_lab/src/main/resources/";
-    protected abstract String processingMethod(FileReader file1, FileReader file2, FileReader file3,
-                                               String fileName1, String fileName2, String fileName3) throws IOException;
+    protected abstract String processingMethod(
+            FileReader file1,
+            FileReader file2,
+            FileReader file3,
+            String fileName1,
+            String fileName2,
+            String fileName3
+    );
 
-    public void processFileContent(String fileName1, String fileName2, String fileName3, String fileName4) throws IOException {
-        FileReader fileReader1 = new FileReader(Paths.get(path, fileName1).toString());
-        FileReader fileReader2 = new FileReader(Paths.get(path, fileName2).toString());
-        FileReader fileReader3 = new FileReader(Paths.get(path, fileName3).toString());
-        FileWriter fileWriter = new FileWriter(Paths.get(path, fileName4).toString());
-        fileWriter.write(processingMethod(fileReader1, fileReader2, fileReader3,fileName1,fileName2,fileName3));
-        fileReader1.close();
-        fileReader2.close();
-        fileReader3.close();
-        fileWriter.close();
+    public void processFileContent(
+            String fileName1,
+            String fileName2,
+            String fileName3,
+            String fileName4
+    ) {
+        try {
+            FileReader fileReader1 = new FileReader(Paths.get(path, fileName1).toString());
+            FileReader fileReader2 = new FileReader(Paths.get(path, fileName2).toString());
+            FileReader fileReader3 = new FileReader(Paths.get(path, fileName3).toString());
+            FileWriter fileWriter = new FileWriter(Paths.get(path, fileName4).toString());
+            fileWriter.write(processingMethod(fileReader1, fileReader2, fileReader3, fileName1, fileName2, fileName3));
+            fileReader1.close();
+            fileReader2.close();
+            fileReader3.close();
+            fileWriter.close();
+        } catch (IOException ex) {
+            System.out.println("Ошибка ввода/вывода");
+        }
     }
 }
--- second_lab/src/main/java/lab/chernyshev/SecondLabApplication.java
+++ second_lab/src/main/java/lab/chernyshev/SecondLabApplication.java
@@ -1,21 +1,22 @@
 package lab.chernyshev;
 
-import java.io.IOException;
+public class SecondLabApplication {
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+
         FileContentProcessor fileContentProcessor1 = new ApplicationOne();
         fileContentProcessor1.processFileContent(
                 "file1.txt",
                 "file2.txt",
                 "file3.txt",
-                "ApplicationOneOutput.txt");
+                "ApplicationOneOutput.txt"
+        );
+
         FileContentProcessor fileContentProcessor2 = new ApplicationTwo();
         fileContentProcessor2.processFileContent(
                 "file1.txt",
                 "file2.txt",
                 "file3.txt",
                 "ApplicationTwoOutput.txt");
-
     }
 }
